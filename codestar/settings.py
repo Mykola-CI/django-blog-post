49de35e2d9fa77a43d7cc8772409be1aeab32b08
Mykola Kuievda <dankraig23@gmail.com>
2024-03-13 14:03:52 +0000

    Set environment variables for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1q8*5i^27afb=9kd%qkq=1ru4ghz3q*0cm^(r4awdf13ssu%c-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
