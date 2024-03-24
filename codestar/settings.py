d45bb047b8bc537e98d0f0650f96c3fd7260ebcd
Mykola Kuievda <dankraig23@gmail.com>
2024-03-24 16:33:59 +0000

    Add forms.py, comment views, comment confirm message

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    'localhost',
+    '192.168.1.207'
     ]
 
 
@@ -49,6 +51,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'blog',
     'about',
@@ -57,6 +61,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
